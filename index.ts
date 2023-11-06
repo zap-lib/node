b62ca8058dbfeff2199d05e04c7072f90a36eec7
parksb <parkgds@gmail.com>
2023-11-06 16:30:24 +0900

    Generate type declaration files to build output

--- index.ts
+++
@@ -1,5 +0,0 @@
-import ZapServer from "./src/server";
-import ZapClient from "./src/client";
-
-export * from "./src/models";
-export { ZapServer, ZapClient };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import ZapServer from "./server";
+import ZapClient from "./client";
+
+export * from "./models";
+export { ZapServer, ZapClient };
