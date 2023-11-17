d44418a65ab49200f97e04af7e7c34392d64b30d
parksb <parkgds@gmail.com>
2023-11-17 22:25:55 +0900

    Lighten payload format of ZapText

--- src/models/TypeSizes.ts
+++ src/models/TypeSizes.ts
@@ -1,4 +1,5 @@
 export enum TypeSizeBytes {
+  BYTE = 1,
   INT = 4,
   FLOAT = 4,
   DOUBLE = 8,
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,5 +2,4 @@ export * from "./Zapable";
 export * from "./ZappObject";
 export * from "./ZappHeader";
 export * from "./ZappPayload";
-export * from "./Charset";
 export * from "./TypeSizes";
